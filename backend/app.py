8c216cc2217f713ee3411de8d7c4207326ae8140
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2024-07-10 10:28:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -265,10 +265,12 @@ def upload_file():
     
     return jsonify({'error': 'Failed to process the file'}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
